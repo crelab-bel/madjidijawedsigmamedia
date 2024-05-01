cfd10fa03961f99c39e3acfabfd9f58659782280
crelab-bel <168651978+crelab-bel@users.noreply.github.com>
2024-05-01 19:25:40 +0200

    Add Base64 encoded file

--- app/content.js
+++ app/content.js
@@ -1,4 +1,4 @@
 export const mainHero = {
-    h1: 'Plaats een SEO vriendelijke Hero tekst hier',
-    paragraaf: 'Plaats een SEO vriendelijke text die de H1 text ondersteunt hier',
+    h1: 'Betrouwbare en Snelle Taxi Service in Gent',
+    paragraaf: 'Ontdek comfort en gemak met onze 24/7 beschikbare taxi's. Waar u ook moet zijn in Gent, onze professionele chauffeurs staan klaar om u veilig en op tijd te vervoeren.',
 };
